155239650d524f9e53bfebfad6d63bea8fee8a77
Killian Gemoets <kilgem@live.fr>
2022-09-02 17:51:46 +0200

    coffee shops

--- discover-coffee-shops/pages/coffee-store/[id].js
+++ discover-coffee-shops/pages/coffee-store/[id].js
@@ -147,7 +147,6 @@ const CoffeeStore = (initialProps) => {
   }, [id, initialProps, initialProps.coffeeStore]);
 
   const handleUpvoteButton = async () => {
-    console.log("vote");
     setVotingCount((count) => count + 1);
     try {
       const response = await fetch("/api/favouriteCoffeeStoreById", {
